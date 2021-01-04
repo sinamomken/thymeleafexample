22c080636475295880bf3f0e0dbfadd94fc0e8bd
Sina Momken <sina.momken@gmail.com>
2021-01-04 17:51:07 +0330

    First commit

---
+++ src/main/java/ir/sinamomken/thymeleafexample/ThymeleafexampleApplication.java
@@ -0,0 +1,13 @@
+package ir.sinamomken.thymeleafexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafexampleApplication.class, args);
+    }
+
+}
